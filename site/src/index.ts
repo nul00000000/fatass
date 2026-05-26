4319b03db2c68088495c6a88eca6db478a5c6726
nul00000000 <dev@monke.gay>
2026-05-26 13:25:58 -0400

    submitting info yay

--- site/src/index.ts
+++ site/src/index.ts
@@ -24,10 +24,12 @@ const groupPass = document.querySelector("#groupPass") as HTMLInputElement;
 const usernameField = document.querySelector("#username") as HTMLInputElement;
 const joinButton = document.querySelector("#join") as HTMLButtonElement;
 
+const repsNum = document.querySelector("#numEx") as HTMLInputElement;
 const exTypeSelect = document.querySelector("#extype") as HTMLSelectElement;
+const exSubmit = document.querySelector("#subEx") as HTMLButtonElement;
 
 const weightField = document.querySelector("#weight") as HTMLInputElement;
-const weightSubmit = document.querySelector("#subWeight") as HTMLInputElement;
+const weightSubmit = document.querySelector("#subWeight") as HTMLButtonElement;
 
 let token: string;
 let username: string;
@@ -48,7 +50,6 @@ function getMostRecentRecord(user: User): DayRecord {
             mostRecent = date;
         }
     }
-    console.log("recent: " + mostRecent);
     return user.records[mostRecent];
 }
 
@@ -83,13 +84,8 @@ function initMain() {
             let resp = JSON.parse(req2.response);
             if(resp.code == 0) {
                 users = resp.users;
-                console.log(users);
-                console.log(username);
                 let user = users[username];
-                console.log(user);
                 let record = getMostRecentRecord(user);
-                console.log(record);
-                console.log(record.weight);
                 weightField.value = "" + record.weight;
             } else {
                 console.log("Group info could not be gotted");
@@ -166,31 +162,48 @@ function setup() {
     weightSubmit.onclick = () => {
         let date = new Date(); //already localized
         
-        let group = groupCode.value;
-        let pass = groupPass.value;
-        username = usernameField.value;
+        let dateString = (date.getDate().toString(10).padStart(2, "0")) + "-" + ((date.getMonth() + 1).toString(10).padStart(2, "0")) + "-" + date.getFullYear();
+        let weight = +weightField.value;
 
         let req = new XMLHttpRequest();
-        req.open("POST", "/fatass/api/login/", true);
+        req.open("POST", "/fatass/api/setweight/", true);
         req.setRequestHeader("Content-Type", "application/json");
         req.onreadystatechange = () => {
             if(req.readyState == 4 && req.status == 200) {
                 let resp = JSON.parse(req.response);
                 if(resp.code == 0) {
-                    token = resp.token;
-                    initMain();
-    
-                    localStorage.setItem("fatassUsername", username);
-                    localStorage.setItem("fatassGroupCode", group);
+                    //process new record TODO
+                    console.log(resp.record);
                 } else {
-                    console.log("login failed: " + resp.error);
+                    console.log("setweight failed: " + resp.error);
                 }
             }
         };
-        req.send(JSON.stringify({username: username, groupCode: group, groupPass: pass}));
+        req.send(JSON.stringify({token: token, weight: weight, date: dateString}));
+    };
 
-        groupFinder.style.display = "none";
-        fatassContainer.style.display = "flex";
+    exSubmit.onclick = () => {
+        let date = new Date(); //already localized
+        
+        let dateString = (date.getDate().toString(10).padStart(2, "0")) + "-" + ((date.getMonth() + 1).toString(10).padStart(2, "0")) + "-" + date.getFullYear();
+        let reps = +repsNum.value;
+        let exType = exTypeSelect.value;
+
+        let req = new XMLHttpRequest();
+        req.open("POST", "/fatass/api/addreps/", true);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.onreadystatechange = () => {
+            if(req.readyState == 4 && req.status == 200) {
+                let resp = JSON.parse(req.response);
+                if(resp.code == 0) {
+                    //process new record TODO
+                    console.log(resp.record);
+                } else {
+                    console.log("addreps failed: " + resp.error);
+                }
+            }
+        };
+        req.send(JSON.stringify({token: token, reps: reps, extype: exType, date: dateString}));
     };
 }
 
